70eaeb55d9ba8983b306bb7f72aafab7371642d3
kebtech <64436886+kebtech@users.noreply.github.com>
2023-09-21 11:13:54 -0400

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, time::Duration};
+use std::{collections::HashMap, time::Duration, fmt::Debug};
 
 use reqwest::header::{HeaderMap, HeaderName};
 use serde::{de::DeserializeOwned, Serialize};
@@ -20,24 +20,38 @@ impl Rest {
         Ok(res)
     }
 
-    pub async fn get_with_auth<T: DeserializeOwned>(url: &str, token: &str) -> anyhow::Result<T> {
+    pub async fn get_with_auth<T: DeserializeOwned + Debug>(url: &str, token: &str) -> anyhow::Result<T> {
         let client = reqwest::Client::new();
         let mut headers = reqwest::header::HeaderMap::new();
         headers.insert(
             "Authorization",
             reqwest::header::HeaderValue::from_str(&format!("token {}", token))?,
         );
+        headers.insert(
+            "User-Agent",
+            reqwest::header::HeaderValue::from_static("cnctd_rest"),
+        );
     
-        let res = client
+        let response = client
             .get(url)
             .headers(headers)
             .send()
-            .await?
-            .json::<T>()
             .await?;
     
-        Ok(res)
+        // Check if the request was successful
+        if response.status().is_success() {
+            let res: T = response.json().await?;
+            // println!("response: {:?}", res);
+            Ok(res)
+        } else {
+            // Print the raw response text for debugging
+            let raw_response = response.text().await?;
+            println!("Raw response: {}", raw_response);
+            Err(anyhow::anyhow!("Received an error response"))
+        }
     }
+    
+    
 
     pub async fn get_with_custom_headers_and_timeout<T: DeserializeOwned>(
         url: &str, 
